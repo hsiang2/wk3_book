8389f8d3bc392f2181c720e0f43eb96bd00705d4
Hsiang <q224399377@gmail.com>
2022-03-20 13:20:34 +0800

    drawer navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,19 +1,95 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DrawerActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Image, Pressable } from 'native-base';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+import { Divider, Image, Pressable, Text, VStack } from 'native-base';
 import HomeScreen from '../screens/HomeScreen';
 import DetailScreen from '../screens/DetailScreen';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 const Navigation = () => {
     return(
         <NavigationContainer>
-          <MyTabs />
+          <MyDrawer />
         </NavigationContainer>
+    );
+}
+
+const CustomDrawerContent = (props) => {
+    return(
+        <DrawerContentScrollView {...props}>
+            <VStack ml={4} mb={2} mt={10} space={4}>
+              <Image 
+                h={12} w={12}
+                source={{uri: "https://compai.pub/v1/png/0d9659ae48bd8d4150d393f13accf457c31eedf64736150133f9166837b46c6b"}}
+              />
+              <Text fontSize={24} color={"#131313"}>May</Text>
+            </VStack>
+            <Divider my={2}/>
+            <DrawerItemList {...props}/>
+        </DrawerContentScrollView>
+    );
+}
+
+const MyDrawer = () => {
+    return (
+        <Drawer.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+                drawerActiveBackgroundColor: "#fff",
+                drawerActiveTintColor: "#666666",
+                drawerInactiveTintColor: "#666666",
+                drawerStyle: { width: 300},
+                drawerLabelStyle: {fontSize: 14}
+            }}
+            drawerContent={props => <CustomDrawerContent {...props}/>}
+        >
+            <Drawer.Screen
+              name='Home'
+              component={MyTabs}
+              options={{
+                  headerShown: false,
+                  drawerIcon: () => <Image source={require("../../assets/icon_home.png")}/>
+              }}
+            />
+            <Drawer.Screen
+              name='Wishlist'
+              component={MyTabs}
+              options={{
+                headerShown: false,
+                drawerIcon: () => <Image source={require("../../assets/icon_nav_bookmark.png")}/>
+            }}
+            />
+            <Drawer.Screen
+              name='MyBooks'
+              component={MyTabs}
+              options={{
+                headerShown: false,
+                drawerLabel: "My Books",
+                drawerIcon: () => <Image source={require("../../assets/icon_mybook.png")}/>
+            }}
+            />
+            <Drawer.Screen
+              name='Account'
+              component={MyTabs}
+              options={{
+                headerShown: false,
+                drawerIcon: () => <Image source={require("../../assets/icon_account.png")}/>
+            }}
+            />
+            <Drawer.Screen
+              name='Setting'
+              component={MyTabs}
+              options={{
+                headerShown: false,
+                drawerIcon: () => <Image source={require("../../assets/icon_settings.png")}/>
+            }}
+            />
+        </Drawer.Navigator>
     );
 }
 
@@ -68,7 +144,7 @@ const MyTabs = () => {
     );
 }
 
-const BookStack = () => {
+const BookStack = ({navigation}) => {
     return(
       <Stack.Navigator
         screenOptions={{
@@ -81,9 +157,13 @@ const BookStack = () => {
           component={HomeScreen}
           options={{
             headerLeft: () => (
-              <Image 
-                source={require("../../assets/icon_menu.png")}
-              />
+                <Pressable
+                  onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+                >
+                    <Image 
+                      source={require("../../assets/icon_menu.png")}
+                    />
+                </Pressable>
             ),
             headerRight: () => (
               <Image 
